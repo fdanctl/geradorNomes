af0b8ff5748ef721af4270b185121351648206b5
Bytes4Future <bytes4future@AKF19-02808.local>
2025-01-13 15:04:39 +0000

    update server

---
+++ server/services/generator.js
@@ -0,0 +1,19 @@
+const { FindApelidos } = require("../data/apelidos");
+const InsertGeneratedNome = require("../data/generater");
+const { FindNomes } = require("../data/nomes");
+
+async function ReadGenerate(obj) {
+  const apelidos = await FindApelidos();
+  const nomes = await FindNomes();
+
+  const nomesIdx = Math.floor(Math.random() * nomes.length);
+  const apelidosIdx = Math.floor(Math.random() * apelidos.length);
+
+  const nomeGerado = `${nomes[nomesIdx].nome} ${apelidos[apelidosIdx].apelido}`;
+
+  await InsertGeneratedNome({ nome: nomeGerado });
+
+  return nomeGerado;
+}
+
+module.exports = ReadGenerate;
