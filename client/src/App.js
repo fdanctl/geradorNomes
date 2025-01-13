81ae9dd04e15b010e28bddc9f9d3793206370d51
OliveiraPedroHS <pedrohsooliveira@gmail.com>
2025-01-13 15:24:53 +0000

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Button from "./componentes/button";
 
 function App() {
   return (
     <div className="App">
-      <h1>Gerador de Nomes</h1>
+      <Button />
     </div>
   );
 }
---
+++ client/src/componentes/button.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./button.css";
+
+export default function Button() {
+  const [nomes, setNomes] = useState("");
+
+  const handlerBtn = async () => {
+    const res = await fetch("http://localhost:3000/API/generator");
+    const data = await res.json();
+    setNomes(data);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>Gerador de Nomes</h1>
+        <h2>{nomes}</h2>
+        <button onClick={async () => await handlerBtn()}>Gerar Nomes</button>
+      </div>
+    </>
+  );
+}
